d7ce75f19df65ed23044f9d3b0d06ebb44d58b1c
Jordan Trickett <jordan1trickett@gmail.com>
2020-03-04 07:47:44 +1100

    projects animated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 console.log('hello world');
+
+
+let scrollDiv;
+let projectDiv;
+let isScrolling = null;
+
+window.onload=function(){
+    scrollDiv = document.querySelector('.work');
+    scrollDiv.addEventListener("wheel", onScroll, false);
+
+    projectDiv = document.querySelectorAll('.project');
+}
+
+
+function onScroll(e) {
+
+    if (event.deltaX > 0) {
+        projectDiv.forEach( item => {
+          item.classList.remove('animate-over');
+          item.classList.add('animate-right');
+        });
+
+      } else {
+
+        projectDiv.forEach( item => {
+          item.classList.remove('animate-over');
+          item.classList.add('animate-left');
+        });
+      }
+
+    clearTimeout(isScrolling);
+    isScrolling = setTimeout(function() {
+
+        projectDiv.forEach( item => {
+          item.classList.add('animate-over');
+          item.classList.remove('animate-right', 'animate-left');
+        });
+    }, 200);
+}
