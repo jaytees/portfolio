c2fc6a5604c2f895e7f3c7f6329f9dcfad78fd11
Jordan Trickett <jordan1trickett@gmail.com>
2020-03-30 17:09:14 +0100

    active nav link

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
 //project animation
 let scrollDiv;
 let projectDiv;
-let isScrolling = null
+let isScrolling = null;
 
 const mq = window.matchMedia( "(max-width: 600px)" );
 
-//smooth scroll
-let navLinkProjects;
-
 window.onload=function(){
+    //project scroll
     scrollDiv = document.querySelector('.work');
     scrollDiv.addEventListener("wheel", onScroll, false);
     projectDiv = document.querySelectorAll('.project');
 
+    //nav scroll
+    const navContainer = document.getElementById('nav');
+    // Get all buttons with class="btn" inside the container
+    const navLinks = navContainer.getElementsByClassName("nav__link");
+    // Loop through the links and add the active class to the current/clicked button
+    for (let i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("nav__link--active");
+
+        // If there's no active class
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(" nav__link--active", "");
+        }
+
+        // Add the active class to the current/clicked button
+        this.className += " nav__link--active";
+      });
+    }//for nav active
+
 
 }
-
 
 const onScroll = (e) => {
 
